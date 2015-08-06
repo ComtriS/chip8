63cbd84a89950bd9e5de237664991e0a3bce30dd
ComtriS <kachikosbodkin@gmail.com>
2015-08-06 00:31:30 -0700

    Debug: add a debug window for stepping

---
+++ debug.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <string.h>
+#include "display.h"
+
+#define DEBUG_X_MIN   -1
+#define DEBUG_X_MAX   (DEBUG_X_MIN + 88)
+#define DEBUG_X_SIZE  (DEBUG_X_MAX - DEBUG_X_MIN)
+#define DEBUG_Y_MIN   33
+#define DEBUG_Y_MAX   (DEBUG_Y_MIN + 10)
+#define DEBUG_Y_SIZE  (DEBUG_Y_MAX - DEBUG_Y_MIN)
+
+bool debug_inited = false;
+
+char debug_buffer[DEBUG_X_MAX * DEBUG_Y_MAX];
+int debug_idx = 0;
+
+void debug_start(void)
+{
+	if (!debug_inited) {
+		display_gotoxy(DEBUG_X_MIN, DEBUG_Y_MIN);
+		debug_inited = true;
+		return;
+	}
+}
+
+int str_count(char* str, char c)
+{
+	int i;
+	for (i=0; str[i]; str[i]==c ? i++ : *str++);
+	return i;
+}
+
+void debug_end(void)
+{
+	int count = str_count(debug_buffer, '\n');
+	if (count > DEBUG_Y_SIZE) {
+		char* next_line = strchr(debug_buffer, '\n') + 1;
+		memmove(debug_buffer, next_line, strlen(next_line)+1);
+	}
+	
+	display_saveCursor();
+	display_gotoxy(DEBUG_X_MIN, DEBUG_Y_MIN);
+	printf("                                            \n");
+	printf("                                            \n");
+	printf("                                            \n");
+	printf("                                            \n");
+	printf("                                            \n");
+	printf("                                            \n");
+	printf("                                            \n");
+	printf("                                            \n");
+	printf("                                            \n");
+	printf("                                            \n");
+	display_gotoxy(DEBUG_X_MIN, DEBUG_Y_MIN);
+	printf("%s", debug_buffer);
+	printf("                                            ");
+	debug_idx = strlen(debug_buffer);
+	display_gotoxy(DEBUG_X_MIN, DEBUG_Y_MAX);
+	display_loadCursor();
+}
---
+++ debug.h
@@ -0,0 +1,17 @@
+#ifndef __DEBUG_H__
+#define __DEBUG_H__
+
+void debug_start(void);
+void debug_end(void);
+
+extern char debug_buffer[];
+extern int debug_idx;
+
+#define printf(...)                                                  \
+	do {                                                             \
+		debug_start();                                               \
+		debug_idx += sprintf(&debug_buffer[debug_idx], __VA_ARGS__); \
+		debug_end();                                                 \
+	} while (0)
+
+#endif /* __DEBUG_H__ */
